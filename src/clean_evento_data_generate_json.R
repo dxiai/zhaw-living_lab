bab5d57e1306e6980694889305622cdce49e6eda
Daniel Bajka <daniel.bajka@gmail.com>
2021-03-21 12:24:16 +0100

    Evento dataset cleaned
    dataset reduced to all modules that are currently active and have data

--- src/clean_evento_data_generate_json.R
+++ src/clean_evento_data_generate_json.R
@@ -8,50 +8,56 @@ if (!require('methods')) install.packages('methods'); library(methods)
 if (!require('stringi')) install.packages('stringi'); library(stringi)
 if (!require('stringr')) install.packages('stringr'); library(stringr)
 if (!require('rlang')) install.packages('rlang'); library(rlang)
+if (!require('plyr')) install.packages('plyr'); library(plyr)
 
 # Set the directory to the application path
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 # temporary working directory
-tmp.dir = paste0(getwd(),"/tmp")
+dir.tmp = paste0(getwd(),"/tmp")
 # set the director< path for the data that shall be shared
 setwd("..")
-data.dir =   paste0(getwd(),"/data")
+dir.data =   paste0(getwd(),"/data")
 # Reset the directory to the application path
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+
 
 # Function that transfroms the earlier labeled raw text into a dataframe
 ModuleDataPreprocessed <- function (my.input){  #,mySelection = c("lernziele","lerninhalt")){
 
-    my.dataframe =  data.frame(matrix(data = NA, nrow = 0, ncol = 0))
+    # debug only
+    # my.input = modules.text[i]
+
+    my.dataFrame =  data.frame(matrix(data = NA, nrow = 0, ncol = 0))
     # my.text <- modulesText[1]
     my.input %>%
         strsplit("///") %>%
         unlist() %>%
         matrix(byrow=TRUE) %>%
-        data.frame(stringsAsFactors=FALSE) -> my.dataset
+        data.frame(stringsAsFactors=FALSE) -> my.dataSet
 
     ## Features in rows
-    my.dataframe =  data.frame(matrix(data = NA, nrow = 0, ncol = 0))
+    # my.dataframe =  data.frame(matrix(data = NA, nrow = 0, ncol = 0))
     # start wirh row 2 because row one always is " "
-    for(i in 2:nrow(my.dataset)){
-        my.dataset[i,] %>%
+    for(i in 2:nrow(my.dataSet)){
+        my.dataSet[i,] %>%
             strsplit(":") -> x
             if(is.na(x)==FALSE){
-                as.data.frame(x) -> my.dataset.split
-                names(my.dataset.split) = ""
-                as.data.frame(t(my.dataset.split)) -> my.dataset.split
-                my.dataframe <- rbindFill(my.dataframe,my.dataset.split)
+                as.data.frame(x) -> my.dataSetSplit
+                names(my.dataSetSplit) = ""
+                as.data.frame(t(my.dataSetSplit)) -> my.dataSetSplit
+                my.dataFrame <- rbind.fill(my.dataFrame,my.dataSetSplit)
             }
             else{
                 break
             }
     }
     if(!is.na(x)){
-        my.dataframe %>%
+        my.dataFrame %>%
             filter(is.na(V1)|is.na(V2)|is.null(V2)|V2 != " ") %>%
             group_by(V1) %>%
+            # across(~paste(., collapse = "//"))  %>%
             summarise_each(funs(paste(., collapse = "//"))) %>%
-            pivot_wider(names_from = V1, values_from = V2) -> my.dataframe
+            pivot_wider(names_from = V1, values_from = V2) -> my.dataFrame
 
         id.title = as.data.frame(c(evento.txt[i,1],evento.txt[i,2])) %>%
             t() %>%
@@ -59,71 +65,71 @@ ModuleDataPreprocessed <- function (my.input){  #,mySelection = c("lernziele","l
         row.names(id.title) = ""
         names(id.title) = c("id","title")
 
-        my.dataframe <- cbind(id.title,my.dataframe)
+        my.dataFrame <- cbind(id.title,my.dataFrame)
     }
-    return(my.dataframe)
+    return(my.dataFrame)
 }
 
 # This function is still a working prototype and does not stucture the JSON in a pretty formt yet
 CreateJSONSubstructure <- function(my.input){
-    my.datatable <- as.data.table(df) %>%
+    my.dataTable <- as.data.table(my.input) %>%
         mutate_at(1,as.character) %>%
         mutate_at(2,as.character)
 
-    my.modul.list = my.course.list = {}
+    my.modulList = my.courselist = {}
     # List of all labels
-    labels <- as.character(unique(my.input$V1))
+    my.inputLabels <- as.character(unique(my.input$V1))
 
-    my.modul.labels <- c("Modul","Datum","Credits","Beschreibung")
-    my.modul.list <- sapply(my.modul.labels, function(x){
-        my.datatable.x <- my.datatable[V1==x, .(V2)]
-        my.datatable.x
+    my.modulLabels <- c("Modul","Datum","Credits","Beschreibung")
+    my.modulList <- sapply(my.modulLabels, function(x){
+        my.dataTable.x <- my.dataTable[V1==x, .(V2)]
+        my.dataTable.x
     })
-    names(my.modul.list) <- my.modul.labels
+    names(my.modulList) <- my.modulLabels
 
     # Create list of all modul related courses
-    my.course.labels <- labels[!(labels %in% my.modul.labels)]
-    my.course.list <- sapply(my.course.labels, function(x){
-        my.datatable.x <- my.datatable[V1==x, .(V2)]
-        my.datatable.x
+    my.courseLabels <- my.inputLabels[!(my.inputLabels %in% my.modulLabels)]
+    my.courseList <- sapply(my.courseLabels, function(x){
+        my.dataTable.x <- my.dataTable[V1==x, .(V2)]
+        my.dataTable.x
     })
 
     # combine list content with list labels
-    names(my.course.list) <- my.course.labels
+    names(my.courseList) <- my.courseLabels
     # create a list in a list by appending the courses as one new Kurse sublist to the module items
-    my.modul.list$Kurse <- my.course.list
+    my.modulList$Kurse <- my.courseList
     # transform the list structure into JSON for further cross plöattform use
-    df.json <- toJSON(my.modul.list,pretty = TRUE, na="null")
+    df.json <- toJSON(my.modulList,pretty = TRUE, na="null")
     # Export the created Evento JSON file
-    write(df.json,  file = paste0(tmp.dir,"/df.JSON"))
+    write(df.json,  file = paste0(dir.tmp,"/df.JSON"))
 }
 
 ################################################################################
 # Main Programm
 #########################################################
 # Read the proprocessed Evento raw data and remove duplicates
-evento_txt <- readRDS(file = paste0(tmp.dir,"/ZHAW_Evento_all_preprocessed.Rda")) %>% distinct()
+evento.txt <- readRDS(file = paste0(dir.tmp,"/ZHAW_Evento_all_preprocessed.Rda")) %>% distinct()
 # Data filter: remove all modules without content
-modulesNoDesc <- filter(evento.txt, grepl('NA', text))
+modules.noDesc <- filter(evento.txt, grepl('NA', text))
 # Data filter: keep all modules with content
-modulesWithDesc <- filter(evento.txt, !grepl('NA', text))
+modules.withDesc <- filter(evento.txt, !grepl('NA', text))
 # Data filter: remove all modules defined as Nachprüfung
-modulesNachprfg <- filter(modulesWithDesc, grepl('Nachprüfung', text))
+modules.nachPrfg <- filter(modules.withDesc, grepl('Nachprüfung', text))
 # Data filter: all remaining datasets are valid and used for further analysis
-modulesRegular <- filter(modulesWithDesc, !grepl('Nachprüfung', text))
+modules.regular <- filter(modules.withDesc, !grepl('Nachprüfung', text))
 # Select the column with text
-modulesText <- modulesRegular[,3]
+modules.text <- modules.regular[,3]
 # Prepare an empty dataframe for the collected and cleaned evento data
-moduleDataframe =  data.frame(matrix(data = NA, nrow = 0, ncol = 0))
-# for (i in 1:10){
-for (i in 1:nrow(modulesRegular)){
-    moduleDataframe <- rbindFill(moduleDataframe,moduleDataPreprocessed(modulesText[i]))
+modules.dataFrame =  data.frame(matrix(data = NA, nrow = 0, ncol = 0))
+for (i in 1:10){
+# for (i in 1:nrow(modules.regular)){
+    modules.dataFrame <- rbind.fill(modules.dataFrame,ModuleDataPreprocessed(modules.text[i]))
     if(round(i/20) == i/20){print(i)}
 }
 # Transform the dataframe into a JSON structure
-ev_mtx_json <- toJSON(as.list(moduleDataframe[1:16]))
+evento.mtxJson <- toJSON(as.list(modules.dataFrame[1:16]))
 # Save the JSON File: Not YET the FINAL PRETTY JSON structure
-write(ev_mtx_json,  file = paste0(tmp.dir,"/ZHAW_Evento_all_cleaned.JSON"))
+write(evento.mtxJson,  file = paste0(dir.tmp,"/ZHAW_Evento_all_cleaned.JSON"))
 
 # Store the digital collection as dataframe into the hidden temp directory
-saveRDS(moduleDataframe, file = paste0(tmp.dir,"/ZHAW_Evento_all_cleaned.Rda"))
+saveRDS(modules.dataFrame, file = paste0(dir.tmp,"/ZHAW_Evento_all_cleaned.Rda"))
--- src/sdg_wordcloud_from_excel.R
+++ src/sdg_wordcloud_from_excel.R
@@ -9,49 +9,51 @@ if (!require('stringi')) install.packages('stringi'); library(stringi)
 
 
 #>>>>>>>>>>>>>>>>>>>> variables initialisation
-tmpDir = ""; dataDir = ""; dataFile = ""; d = ""; sdg_1 = {}
+tmp.dir = ""; data.dir = ""; data.file = ""; d = ""; sdg_1 = {}
 
 
 #>>>>>>>>>>>>>>>>>>>> directories initialization
 # Set the directory to the application path
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 # temporary working directory
-tmpDir = paste0(getwd(),"/tmp")
+tmp.dir = paste0(getwd(),"/tmp")
 # set the director< path for the data that shall be shared
 setwd('..')
-dataDir =  paste0(getwd(),'/data')
+data.dir =  paste0(getwd(),'/data')
 # reset all open dev channels
 graphics.off()
 
 
 #>>>>>>>>>>>>>>>>>>>> Single sdg read from Excel original
-read_sdg_xlsx <- function(mypath, lang, sdgNo){
+ReadSdgXlsx <- function(my.path, my.lang, my.sdgNo){
+    # debugging only
+    # my.path = data.dir; my.lang = "de"; my.sdgNo = 1
     library(readxl)
-    tmp <-read_xlsx(paste0(dataDir,eval(paste0("/SDG",sdgNo,".xlsx"))),sheet = lang, na = 'NULL')[-1,paste0(lang,"_full")] %>%
+    my.tmp <-read_xlsx(paste0(data.dir,eval(paste0("/SDG",my.sdgNo,".xlsx"))),sheet = my.lang, na = 'NULL')[-1,paste0(my.lang,"_full")] %>%
         # tmp <-read_xlsx(paste0(dataDir,eval(paste0("/SDG",sdgNo,".xlsx"))),sheet = lang, na = 'NULL')[2:nrow(dataset),paste0(lang,"_full")] %>%
         as_tibble() %>%
         # clean the column by replacing NAs to ""
         mutate_at(c(1:1), ~replace(., is.na(.), ''))
     # rename column 1
-    names(tmp) = "key"# %>%
+    names(my.tmp) = "key"# %>%
     # duplicate column, first key, second value -> display key as name in the wordcloud
-    tmp <- mutate(tmp,"value" = key) #%>%
+    my.tmp <- mutate(my.tmp,"value" = key) #%>%
     # # create an sdg_x dictionary
-    return(dictionary(setNames(as.list(tmp$key), tmp$value)))
+    return(dictionary(setNames(as.list(my.tmp$key), my.tmp$value)))
 }
 
 
 #>>>>>>>>>>>>>>>>>>>>Load preprocessed Evento dataframe into memory
-dataFile <- readRDS(file = paste0(tmpDir,"/ZHAW_Evento_all_preprocessed.Rda"))
+data.file <- readRDS(file = paste0(tmp.dir,"/ZHAW_Evento_all_preprocessed.Rda"))
 # crerate the corpus
-corp = corpus(dataFile,text_field = "text") # , docvars = "id")
+corp = corpus(data.file,text_field = "text") # , docvars = "id")
 
-doc_vars = docvars(corp)
-head(doc_vars)
+doc.vars = docvars(corp)
+head(doc.vars)
 # No stemming since the words should be compared in their full lenght
-dtm_full = dfm(corp, tolower=T, remove = stopwords('de'), stem = F, remove_punct=T)
+dtm.full = dfm(corp, tolower=T, remove = stopwords('de'), stem = F, remove_punct=T)
 # replace feacture counts per document > 1 to 1. Achieve a boolean dtm
-dtm_bool <- dfm_weight(dtm_full, scheme = "boolean")
+dtm.bool <- dfm_weight(dtm.full, scheme = "boolean")
 # dtm = dfm_trim(dtm_bool, min_termfreq = 1000 )
 
 #>>>>>>>>>>>>>>>>>>>> Loop over n SDG Excel files and save the created wordclouds
@@ -59,15 +61,17 @@ for (no in 1:1){
     tryCatch(
         {
             # assign the result of function read_sdg_xlsx to a dymamically generated variable
-            assign(paste0("sdg_",no),read_sdg_xlsx(dataDir,"de",no))
+            assign(paste0("sdg_",no),ReadSdgXlsx(data.dir,"de",no))
             # create the wordcloud
-            dict_dtm = dfm_lookup(dtm_bool,eval(parse(text=paste0("sdg_",no))), exclusive = TRUE)
-            dict_trl = convert(dict_dtm, to = "tripletlist") %>%
-                as.tibble() %>%
+            dict.dtm = dfm_lookup(dtm.full,eval(parse(text=paste0("sdg_",no))), exclusive = TRUE)
+            # dict.dtm = dfm_lookup(dtm.bool,eval(parse(text=paste0("sdg_",no))), exclusive = TRUE)
+            dict.trl = convert(dict.dtm, to = "tripletlist") %>%
+                as_tibble() %>%
                 # add column with a reference to the line numbers corresponding to doc_vars
                 mutate(idx = str_replace_all(document,"text",''))
 
-            dd = cbind(dict_trl,doc_vars[as.numeric(dict_trl$idx),])
+            dd = cbind(dict.trl,doc.vars[as.numeric(dict.trl$idx),])
+            # assign(paste0("df_sdg_",no),read_sdg_xlsx(data.dir,"de",no))
 
 
                 # # create a dtm dataframe
@@ -83,14 +87,14 @@ for (no in 1:1){
             # Sys.sleep(1)
         },
         error=function(cond) {
-            dev.off()
+            # dev.off()
             message("Here's the original error message:")
             message(cond)
             # Choose a return value in case of error
             return(NA)
         },
         warning=function(cond) {
-            dev.off()
+            # dev.off()
             message("Here's the original warning message:")
             message(cond)
             # Choose a return value in case of warning
@@ -107,3 +111,8 @@ for (no in 1:1){
         }
     )
 }
+
+
+
+
+df2 = group_by(dd,"document")
